4ac76840b3fd7a99e8e4d1607065bcc85aeba00b
Matthew <matthewleemccane@gmail.com>
2025-07-26 18:21:18 -0500

    Initial Commit

---
+++ SunDevilMeet/SunDevilMeetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunDevilMeetApp.swift
+//  SunDevilMeet
+//
+//  Created by Matthew McCane on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct SunDevilMeetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
